3a8e7206be2d05e01696f92aa656ba62d66be48d
Maina Wycliffe <wmmaina7@gmail.com>
2018-08-15 14:39:56 +0300

    added service to call github api

---
+++ src/app/services/github.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { GithubResponse, Items } from '../interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+  constructor(private http: HttpClient) {}
+
+  search(query: string): Observable<GithubResponse> {
+    const url = 'https://api.github.com/search/repositories';
+    return this.http
+      .get<GithubResponse>(url, {
+        observe: 'response',
+        params: {
+          q: query,
+          sort: 'stars',
+          order: 'desc'
+        }
+      })
+      .pipe(
+        map((res: Response) => {
+          return res.body;
+        })
+      );
+  }
+}

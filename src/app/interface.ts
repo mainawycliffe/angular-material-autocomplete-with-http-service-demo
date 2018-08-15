ec80b3ee29ee626c094946f0abb8e9f079b209cb
Maina Wycliffe <wmmaina7@gmail.com>
2018-08-15 14:39:37 +0300

    added interface for github return object

---
+++ src/app/interface.ts
@@ -0,0 +1,44 @@
+export interface Item {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  owner: Owner;
+  private: boolean;
+  html_url: string;
+  description: string;
+  fork: boolean;
+  url: string;
+  created_at: Date;
+  updated_at: Date;
+  pushed_at: Date;
+  homepage: string;
+  size: number;
+  stargazers_count: number;
+  watchers_count: number;
+  language: string;
+  forks_count: number;
+  open_issues_count: number;
+  master_branch: string;
+  default_branch: string;
+  score: number;
+}
+
+export interface Owner {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  received_events_url: string;
+  type: string;
+}
+
+export declare type Items = Item[];
+
+export interface GithubResponse {
+  total_count: number;
+  incomplete_results: boolean;
+  items: Items;
+}

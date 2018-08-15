add29047b141345786523e4fa3fe6e0a34cbca3d
Maina Wycliffe <wmmaina7@gmail.com>
2018-08-15 14:40:42 +0300

    setup UI for the demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Items, Item, GithubResponse } from './interface';
+import { GithubService } from './services/github.service';
+import { Observable, observable, of } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import {
+  startWith,
+  map,
+  debounceTime,
+  mergeMapTo,
+  mergeMap,
+  switchMap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-material-autocomplete-with-http-service-demo';
+export class AppComponent implements OnInit {
+  public githubAutoComplete$: Observable<Items> = null;
+  public autoCompleteControl = new FormControl();
+
+  constructor(private githubService: GithubService) {}
+
+  lookup(value: string): Observable<Items> {
+    return this.githubService
+      .search(value.toLowerCase())
+      .pipe(map(results => results.items));
+  }
+
+  ngOnInit() {
+    this.githubAutoComplete$ = this.autoCompleteControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300),
+      switchMap(value => {
+        if (value !== '') {
+          return this.lookup(value);
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  MatAutocompleteModule,
+  MatInputModule,
+  MatFormFieldModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
